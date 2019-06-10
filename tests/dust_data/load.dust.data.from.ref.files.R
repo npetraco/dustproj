b89b580d5c2b07fd266ff0200a0be8db31929317
npetraco <npetraco@gmail.com>
2019-06-10 16:00:39 -0400

    Remote update

--- tests/dust_data/load.dust.data.from.ref.files.R
+++ tests/dust_data/load.dust.data.from.ref.files.R
@@ -8,6 +8,18 @@ in.prefix <- "tests/dust_data/ref_formats/ref_format_"
 in.fpths <- paste0(in.prefix,fnmes)
 master.dust.info <- load.datasheets(in.fpths)
 
-rowSums(master.dust.info$master.indicator.mat)
-plot(1:nrow(master.dust.info$master.indicator.mat), rowSums(master.dust.info$master.indicator.mat), typ="h")
+all.counts <- rowSums(master.dust.info$master.indicator.mat)
+all.categ  <- master.dust.info$master.category.mat
+data.frame(all.categ, all.counts)
+plot(1:length(all.counts), all.counts, typ="h", xlab="Categ. #", ylab="count")
+points(1:length(all.counts), rep(0,length(all.counts)), col=as.factor(all.categ[,1]))
 
+# Drop the categories that never occured:
+zero.idxs <- which(all.counts==0)
+plot(1:length(all.counts[-zero.idxs]), all.counts[-zero.idxs], typ="h", xlab="Categ. #", ylab="count")
+points(1:length(all.counts[-zero.idxs]), rep(0,length(all.counts[-zero.idxs])), col=as.factor(all.categ[-zero.idxs,1]))
+
+data.frame(all.categ, master.dust.info$master.indicator.mat)
+
+write.csv(data.frame(all.categ, master.dust.info$master.indicator.mat), "tests/dust_data/dust.data.as-of-6-10-19.csv", row.names = F)
+head(fdat)
